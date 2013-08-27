eaa9ad3f56cabb903fce83c31b5fdcede943b876
dolfinus <martinov_m_s_@mail.ru>
2013-08-28 00:35:36 +0400

    Version 0.0.6
    Usage ~400 Mb RAM.
    
    In this version I tried to move most of programs into separate wim file which will be mounted at b:\programs dir at boot to reduce RAM usage.
    Such version must be a fork of main version, but I`l do it next time.
    
    Explorer.exe don`t works.

--- boot.wim/Program Files/Total Commander/Plugins/exe/TweakTC.bat
+++
@@ -1,2 +0,0 @@
-copy C:\Windows\System32\msvbvm60.dll /v /a /b
-C:\Program Files\Total Commander\Plugins\exe\msvbvm60.dll /v /a /b
--- boot.wim/Windows/system32/tweaks.bat
+++ boot.wim/Windows/system32/tweaks.bat
@@ -1,6 +1,6 @@
 regedit /s Y:\sources\reg\sys.reg
+regedit /s Y:\sources\reg\software.reg
+"X:\Program Files\7-Zip\7z.exe" -o "y:\sources\programs.wim" "x:\program files"
 regsvr32 /s Query.dll
 regsvr32 /s "X:\Program Files\HashTab\HashTab32.dll"
-regsvr32 /s "X:\Program Files\ExtremeCopy\XCShellExt.dll"
-regsvr32 /s ieproxy.dll
 exit
