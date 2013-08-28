413db9d2d714b2120bd8d83155c9439751fb06a5
dolfinus <martinov_m_s_@mail.ru>
2013-08-28 11:53:10 +0400

    Version 0.0.7.6
    Added BatteryEater, DesktopLocker, DesktopOK, DevManView, Everything, MyLanViewer, PasswrdNotebook, ResetHardware, ATTO Disk Benchmark, some fonts.
    
    Deleted DC-pass, DialupPE, FileZilla, Virtual Machine additions, eXPressor, XNView ShellExt.
    
    Also added Microsoft ERD utils and updated Start menu. Updated Deffragler.

--- boot.wim/Program Files/Total Commander/Plugins/exe/TweakTC.bat
+++ boot.wim/Program Files/Total Commander/Plugins/exe/TweakTC.bat
@@ -1,2 +1,1 @@
-copy C:\Windows\System32\msvbvm60.dll /v /a /b
-C:\Program Files\Total Commander\Plugins\exe\msvbvm60.dll /v /a /b
+copy %systemdrive%\Program Files\Total Commander\Plugins\exe\msvbvm60.dll %systemdrive%\Windows\System32\msvbvm60.dll /v /a /b
--- boot.wim/Windows/system32/tweaks.bat
+++ boot.wim/Windows/system32/tweaks.bat
@@ -1,12 +1,17 @@
-regedit /s Y:\sources\reg\sys.reg
-regsvr32 /s Query.dll
-regsvr32 /s "X:\Program Files\HashTab\HashTab32.dll"
-regsvr32 /s "X:\Program Files\ExtremeCopy\XCShellExt.dll"
-regsvr32 /s ieproxy.dll
-regsvr32 /s "X:\Program Files\ImageX\gimagex_com.dll"
-regsvr32 /s "X:\Program Files\XNView\XnViewShellExt.dll"
-pnputil -i -a "X:\Program Files\ImageX\wimfltr.inf"
 imdisk -a -s 20%% -m B: -p "/fs:fat32 /q /y"
 mkdir "b:\temp"
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon /v "Shell"
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon /v "Shell" /d "explorer.exe" /t REG_SZ
+if EXIST "b:\temp" regedit /s "%systemroot%\system32\temp.reg"
+regedit /s Y:\sources\reg\sys.reg
+regsvr32 /i /s Query.dll
+regsvr32 /i /s "%systemdrive%\Program Files\HashTab\HashTab32.dll"
+regsvr32 /i /s "%systemdrive%\Program Files\ExtremeCopy\XCShellExt.dll"
+regsvr32 /i /s ieproxy.dll
+regsvr32 /i /s "%systemdrive%\Program Files\XNView\XnViewShellExt.dll"
+pnputil -i -a "%systemdrive%\Program Files\ImageX\wimfltr.inf"
+"%systemdrive%\Program Files\SpecChar\SpecChar.exe"
+"%systemdrive%\Program Files\Yandex\Punto Switcher\punto.exe"
+"%systemdrive%\Program Files\DesktopOK\DesktopOK.exe"
+"%systemdrive%\Program Files\Everything\Everything.exe" -startup
+cd /d "B:\Programs\"
+"%systemdrive%\Program Files\7-Zip\7z.exe" x "y:\programs\autoit3.7z"
+regsvr32 /i /s "B:\Programs\AutoIt3\autoit3.reg"
