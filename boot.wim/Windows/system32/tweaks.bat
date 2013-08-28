5340cc76f7d69c5d7418841c9ab8cb04e957e490
dolfinus <martinov_m_s_@mail.ru>
2013-08-28 11:09:27 +0400

    Version 0.0.7.3
    Some registry keys deleted.
    Explorer don`t works without file sys.reg, located on LiveCD disk in sources/reg folder.

--- boot.wim/Windows/system32/tweaks.bat
+++ boot.wim/Windows/system32/tweaks.bat
@@ -1,4 +1,3 @@
-monitor.exe
 regedit /s Y:\sources\reg\sys.reg
 regsvr32 /s Query.dll
 regsvr32 /s "X:\Program Files\HashTab\HashTab32.dll"
@@ -7,6 +6,7 @@ regsvr32 /s ieproxy.dll
 regsvr32 /s "X:\Program Files\ImageX\gimagex_com.dll"
 regsvr32 /s "X:\Program Files\XNView\XnViewShellExt.dll"
 pnputil -i -a "X:\Program Files\ImageX\wimfltr.inf"
+monitor.exe
 imdisk -a -s 20%% -m B: -p "/fs:fat32 /q /y"
 mkdir "b:\temp"
 
