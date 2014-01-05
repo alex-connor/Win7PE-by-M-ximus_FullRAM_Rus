3ce8a4894e1de7fbbc735b92fe71085efd8afc5c
dolfinus <martinov_m_s_@mail.ru>
2014-01-05 20:32:43 +0400

    Version 0.1.0.5 beta
    Deleted useless dll files.
    
    Some dlls were reduced

--- boot.wim/Windows/system32/tweaks.bat
+++ boot.wim/Windows/system32/tweaks.bat
@@ -1,8 +1,6 @@
 reg import "x:\windows\system32\explorer.reg" 
 imdisk -a -s 20%% -m B: -p "/fs:fat32 /q /y"
 mkdir "b:\temp"
-sc create AoE binPath= "x:\windows\system32\drivers\aoe32.sys" type= kernel start= boot group= "SCSI Class"
-sc create HTTPDisk binPath= "x:\windows\system32\drivers\wvhttp32.sys" type= kernel start= boot group= "SCSI Class"
 if not EXIST "B:\Programs\" mkdir "B:\Programs\"
 if EXIST "b:\temp" regedit /s "%systemroot%\system32\temp.reg" 
 if not EXIST "b:\temp" imdisk -D -m B:
