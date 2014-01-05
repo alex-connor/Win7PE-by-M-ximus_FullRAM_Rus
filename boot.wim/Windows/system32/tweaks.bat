b78806e58598043e2942a08d7cbf7fe19143495b
dolfinus <martinov_m_s_@mail.ru>
2014-01-05 23:56:41 +0400

    Version 0.1.1.8_7 beta
    Added sh.exe and zPNP.exe for .wim mounting, some system manifest files (for old progs)
    
    Cleaned TotalCommander configuretion files
    
    Registry tweaks

--- boot.wim/Windows/system32/tweaks.bat
+++ boot.wim/Windows/system32/tweaks.bat
@@ -1,10 +1,11 @@
 reg import "x:\windows\system32\explorer.reg" 
 imdisk -a -s 20%% -m B: -p "/fs:fat32 /q /y"
 mkdir b:\temp
-IF not EXIST "B:\Programs\" mkdir "B:\Programs\"
 IF not EXIST b:\temp imdisk -D -m B:
 IF EXIST b:\temp regedit /s "x:\windows\system32\temp.reg"
+if EXIST b:\ set RamDrv=b: && mklink /j "b:\" "b:\peprograms"
 regsvr32 /i /s "x:\Windows\system32\npswf32.dll"
+
 
 
 
