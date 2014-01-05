b78ee7af3b3e2a18de62ac96ff4c7a27be807921
dolfinus <martinov_m_s_@mail.ru>
2014-01-05 23:51:43 +0400

    Version 0.1.1.8_6 beta
    Deleted Mkey, EsetHFSReader
    
    Updated AVZ

--- boot.wim/Windows/system32/tor_disable.cmd
+++ boot.wim/Windows/system32/tor_disable.cmd
@@ -1,8 +1,10 @@
 tor -service stop
 sc delete tor
 reg add "HKEY_LOCAL_MACHINE\software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable  /t REG_DWORD /d 0 /f
-move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xml" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\tor.xml"
+move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xml" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xmlfoxyproxy.xml_tor"
 move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.old.xml" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xml"
+move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.js" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.js_tor"
+move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.old.js" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.js"
 del /f "X:\Windows\system32\config\systemprofile\Desktop\Остановить*"
 xcopy /y "X:\Windows\system32\config\systemprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Сеть\Tor\Запустить*" "X:\Windows\system32\config\systemprofile\Desktop\"
 taskkill /f /im polipo.exe
--- boot.wim/Windows/system32/tor_enable.cmd
+++ boot.wim/Windows/system32/tor_enable.cmd
@@ -4,7 +4,9 @@ tor -service install -f "%systemroot%\system32\drivers\etc\torrc"
 tor -service start
 reg add "HKEY_LOCAL_MACHINE\software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable  /t REG_DWORD /d 1 /f
 move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xml" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.old.xml"
-move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\tor.xml" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xml"
+move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xml_tor" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\foxyproxy.xml"
+move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.js" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.old.js"
+move /y "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.js_tor" "%userprofile%\AppData\Roaming\Mozilla\Firefox\Profiles\bmmf16lz.default\prefs.js"
 del /f "X:\Windows\system32\config\systemprofile\Desktop\Запустить*"
 xcopy /y "X:\Windows\system32\config\systemprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Сеть\Tor\Остановить*" "X:\Windows\system32\config\systemprofile\Desktop\"
 start /b polipo -c "%systemroot%\system32\drivers\etc\resolv.conf"
